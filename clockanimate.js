ab9732ce59d6e1d4b44dc3bb9101f64c3962ebc4
Peshalanyy <65792034+Peshalanyy@users.noreply.github.com>
2025-05-22 18:42:07 +0300

    Delete clockanimate.js

--- clockanimate.js
+++
@@ -1,7 +0,0 @@
-function animateClock(newTime) {
-  const clock = document.getElementById("clock");
-  clock.classList.remove("fade");
-  void clock.offsetWidth; // trigger reflow
-  clock.classList.add("fade");
-  clock.textContent = `${cityCode} — ${newTime}`;
-}
